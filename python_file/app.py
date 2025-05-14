03cfe4c35c31cb64bdffa4afe87f56f1eb82f0fd
7Nayy <nadim.benkrouidem@gmail.com>
2025-05-14 10:18:19 +0200

    CI-CD

--- python_file/app.py
+++ python_file/app.py
@@ -311,4 +311,4 @@ def internal_error(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=os.getenv('FLASK_DEBUG', 'False').lower() == 'true')
+    app.run(host='0.0.0.0', debug=os.getenv('FLASK_DEBUG', 'False').lower() == 'true')
